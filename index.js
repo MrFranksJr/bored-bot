bb638ab02c3dc7e3f3ebdf34ab05b1463050126a
MrFranksJr <frankyjr@me.com>
2023-02-22 18:59:23 +0100

    loading optimization

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ import buttonArray from "/data/buttonArray.js"
 import { loadingSvg, randomIntFromInterval } from "/data/utils.js"
 
 const doBtn = document.getElementById('do-btn')
+let oldIndex = 0
 
 doBtn.addEventListener("click", getActivity)
 
 function getActivity() {
+  doBtn.innerHTML = loadingSvg
   fetch("https://apis.scrimba.com/bored/api/activity")
       .then(response => response.json())
       .then(data => {
-        doBtn.innerHTML = loadingSvg
+        let newIndex = randomIntFromInterval(0, buttonArray.length-1)
+        while (newIndex === oldIndex) {
+          console.log(newIndex)
+          newIndex = randomIntFromInterval(0, buttonArray.length-1)
+        }
         document.getElementById("activity-name").textContent = data.activity
-        doBtn.innerHTML = buttonArray[randomIntFromInterval(0, buttonArray.length-1)]
+        doBtn.innerHTML = buttonArray[newIndex]
+        oldIndex = newIndex
       })
 }
