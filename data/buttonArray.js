edc51ccde46dbad7c00557c0d450a9adf057cedc
MrFranksJr <frankyjr@me.com>
2023-02-22 15:21:00 +0100

    first commit

---
+++ data/buttonArray.js
@@ -0,0 +1,9 @@
+const buttonArray = [
+    "I'm bored!", 
+    "Still bored?", 
+    "Surely you're ok now?",
+    "Something else?",
+    "Now what?"
+]
+
+export default buttonArray
--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-document.getElementById('do-btn').addEventListener("click", getActivity)
+import buttonArray from "./data/buttonArray"
+import { loadingSvg, randomIntFromInterval } from "./data/utils"
+
+const doBtn = document.getElementById('do-btn')
+
+doBtn.addEventListener("click", getActivity)
 
 function getActivity() {
   fetch("https://apis.scrimba.com/bored/api/activity")
-     .then(response => response.json())
-     .then(data => {
-         console.log(data)
-         document.getElementById("activity-name").textContent = data.activity
-     })
+      .then(response => response.json())
+      .then(data => {
+        doBtn.innerHTML = loadingSvg
+        console.log(data)
+        document.getElementById("activity-name").textContent = data.activity
+        doBtn.innerHTML = buttonArray[randomIntFromInterval(0, buttonArray.length-1)]
+      })
 }
